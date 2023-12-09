0c843ec8c45ca4dc639e8fcf077a6ef6189f84c8
sugyeong-lee <101383749+sugyeong-lee@users.noreply.github.com>
2023-12-09 16:40:00 +0900

    version3(UI 편집분리)
    할일목록에 대한 수정,삭제 부분 UI분리

--- TODOAPP/src/TodoListEdit.java
+++ TODOAPP/src/TodoListEdit.java
@@ -1,19 +1,24 @@
-/*import javax.swing.*;
+import javax.swing.*;
 import java.awt.*;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
 public class TodoListEdit {
-	
 	private TodoItem todoItem;
     private JPanel panel;
     private JButton editButton;
     private JButton deleteButton;
+    private JFrame frame;
+    private List<TodoItem> todoItems;
     
-    public TodoItemEdit(TodoItem todoItem) {
+    public TodoListEdit(TodoItem todoItem) {
         this.todoItem = todoItem;
         this.panel = todoItem.getPanel();
         createButtons();
     }
+
     private void createButtons() {
         editButton = new JButton("Edit");
         editButton.addActionListener(e -> todoItem.editItem());
@@ -23,4 +28,6 @@ public class TodoListEdit {
         deleteButton.addActionListener(e -> todoItem.confirmDeletion());
         panel.add(deleteButton);
     }
-}*/
+    
+    
+}
